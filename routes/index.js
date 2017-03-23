60a78b6308195719d632be8b3986613c011f2084
Lisa Ma <limawebdev@gmail.com>
2017-03-23 10:13:32 -0600

    bestbestbest

--- routes/index.js
+++ routes/index.js
@@ -20,31 +20,6 @@ router.post('/', function(req, res, next){
       if(err){
         throw err;
       }else{
-        // var jsonData = JSON.parse(data);
-        // var exists = false;
-        // for(var i = 0; i < jsonData.length; i++){
-        //   var name = jsonData[i].name;
-        //   if(name === user){
-        //     exists = true;
-        //     var updatedPoints = parseInt(jsonData[i].points)+5;
-        //     jsonData[i].points = updatedPoints;
-        //   }
-        // }
-        // if(!exists){
-        //   jsonData.push({
-        //     name: user,
-        //     points: 15
-        //   });
-        // }
-        // fs.writeFile(usersPath, JSON.stringify(jsonData), (err) => {
-        //   if(err){
-        //     throw err;
-        //   }
-        // })
-        // jsonData.sort((a,b) => {
-        //   return b.points - a.points;
-        // })
-        // var topThree = jsonData.slice(0, 3);
         res.redirect(`/question/${user}`);
       }
     })
